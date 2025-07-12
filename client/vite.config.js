0b30b397001dc33cfafe276b11973c9d220bd8e4
musharraf10 <skmusharaf00@gmail.com>
2025-07-12 18:56:19 +0530

    redirects

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [{ src: "public/_redirects", dest: "." }],
-    }),
-  ],
-  build: { outDir: "dist" },
+  plugins: [react()],
   base: "/",
+  build: {
+    outDir: "dist",
+  },
 });
